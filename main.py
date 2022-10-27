f621d134a0e37e280c2d65bd75dac5a455511e53
Gideon Balogun <balogungideon12@gmail.com>
2022-10-27 11:52:45 +0100

    add main file

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def myinfo():
+    return jsonify({'slackUsername': 'itzgeebee',
+                    'backend': True,
+                    'age': 26,
+                    'bio':
+                        'I am a backend developer who loves to code in Python and Javascript'})
+
+if __name__ == '__main__':
+    app.run(debug=False)
